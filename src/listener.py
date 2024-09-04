9d77d499af5d782ec04bdecbccd8763d29eed250
Rafou2898 <rafael.dousse@heig-vd.ch>
2024-09-04 11:29:43 +0200

    fix: Direct response from flask to arduino if data received

--- src/listener.py
+++ src/listener.py
@@ -3,31 +3,41 @@ import requests
 
 app = Flask(__name__)
 
-# Route pour recevoir les requêtes POST sur le port 80
+# Route for receiving POST requests
 @app.route('/sensor-data', methods=['POST'])
 def receive_data():
     data = request.json
     print("Data received:", data)
+
+    # Check if data is valid
     if not data:
         print("Received an invalid or empty JSON request")
         return jsonify({"error": "Invalid JSON data"}), 400
-    
-    print("Data received:", data)
+
+    # Check if the ID is present
     if 'id' in data:
         print("Data received from sensor id:", data['id'])
     else:
         return jsonify({"error": "No id in data"}), 400
 
-    # Transmit the data to the backend via HTTPS
+    # Immediately send 200 status back to the Arduino
+    # This tells the Arduino that the data was received successfully
+    response_arduino = jsonify({"message": "Data successfully received"}), 200
+
+    
     backend_url = "http://backend-phi-drab-76.vercel.app/api/v1/sensors/" 
     backend_url += str(data['id'])
+    
+    # Send data to backend and print backend response
     response = requests.patch(backend_url, json=data, verify=False)
 
     if response.status_code == 200:
-        print(response.json())
-        return jsonify({"message": "Data successfully forwarded to backend"}), 200
+        print("Data forwarded to backend successfully:", response.json())
     else:
-        return jsonify({"error": "Failed to forward data"}), 500
+        print("Failed to forward data to backend:", response.status_code)
+
+    # Return the response to Arduino 
+    return response_arduino
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
